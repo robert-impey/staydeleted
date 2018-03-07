f9d801a4ac1e64866497c9a33b24534ae844bc99
Robert Impey <robert-impey@users.noreply.github.com>
2018-03-07 12:00:46 +0000

    Logging for repeat runs.

--- cmd/sweep.go
+++ cmd/sweep.go
@@ -18,6 +18,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
+	"io"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -30,6 +31,10 @@ type ActionForFile struct {
 
 var NumRepeats int
 var Period int32
+var LogsDir string
+
+var OutWriter io.Writer
+var ErrWriter io.Writer
 
 // sweepCmd represents the sweep command
 var sweepCmd = &cobra.Command{
@@ -49,15 +54,54 @@ func init() {
 		"The number of times to repeat the sweeping.")
 	sweepCmd.Flags().Int32VarP(&Period, "period", "p", 3600,
 		"The number of seconds in the waiting period. A random time during the period is chosen.")
+	sweepCmd.Flags().StringVarP(&LogsDir, "logs", "l", "logs",
+		"The logs directory for repeated runs.")
 }
 
 func sweep(paths []string) {
 	if NumRepeats < 1 {
+		OutWriter = os.Stdout
+		ErrWriter = os.Stderr
+
 		sweepPaths(paths)
 	} else {
+		rootLogFolder, err := filepath.Abs(LogsDir)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
+
+		if _, err := os.Stat(rootLogFolder); os.IsNotExist(err) {
+			fmt.Printf("Making root logs directory '%v'\n", rootLogFolder)
+			os.Mkdir(rootLogFolder, 0755)
+		}
+
+		sdLogFolder := filepath.Join(rootLogFolder, "staydeleted")
+		if _, err := os.Stat(sdLogFolder); os.IsNotExist(err) {
+			fmt.Printf("Making staydeleted logs directory '%v'\n", sdLogFolder)
+			os.Mkdir(sdLogFolder, 0755)
+		}
+
+		timeStr := time.Now().Format("2006-01-02_15.04.05")
+		outLogFileName := filepath.Join(sdLogFolder, fmt.Sprintf("%s.log", timeStr))
+		errLogFileName := filepath.Join(sdLogFolder, fmt.Sprintf("%s.err", timeStr))
+
+		outLogFile, err := os.Create(outLogFileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
+		OutWriter = outLogFile
+
+		errLogFile, err := os.Create(errLogFileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
+		ErrWriter = errLogFile
+
 		for i := 0; i < NumRepeats; i++ {
 			firstWait := rand.Int31n(Period)
 			time.Sleep(time.Duration(firstWait) * time.Second)
+			fmt.Fprintf(OutWriter, "Run: %d at %s\n", i,
+				time.Now().Format("2006-01-02 15:04:05"))
 			sweepPaths(paths)
 			secondWait := Period - firstWait
 			time.Sleep(time.Duration(secondWait) * time.Second)
@@ -70,23 +114,23 @@ func sweepPaths(paths []string) {
 
 		stat, err := os.Stat(path)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
+			fmt.Fprintf(ErrWriter, "%v\n", err)
 			continue
 		}
 
 		if stat.IsDir() {
 			err := sweepDirectory(path)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%v\n", err)
+				fmt.Fprintf(ErrWriter, "%v\n", err)
 			}
 		} else {
 			err := sweepFrom(path)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%v\n", err)
+				fmt.Fprintf(ErrWriter, "%v\n", err)
 			}
 		}
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
+			fmt.Fprintf(ErrWriter, "%v\n", err)
 		}
 	}
 }
@@ -96,7 +140,7 @@ func sweepFrom(sweepFromFileName string) error {
 	defer sweepFromFile.Close()
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to open file to sweep from: %v\n", err)
+		fmt.Fprintf(ErrWriter, "Unable to open file to sweep from: %v\n", err)
 		return err
 	}
 
@@ -105,7 +149,7 @@ func sweepFrom(sweepFromFileName string) error {
 		directoryToSweep := input.Text()
 		err := sweepDirectory(directoryToSweep)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Unable to sweep from: '%v' - '%v'\n", directoryToSweep, err)
+			fmt.Fprintf(ErrWriter, "Unable to sweep from: '%v' - '%v'\n", directoryToSweep, err)
 			continue
 		}
 	}
@@ -116,46 +160,46 @@ func sweepFrom(sweepFromFileName string) error {
 func sweepDirectory(directoryToSweep string) error {
 	var absDirectoryToSweep, err = filepath.Abs(directoryToSweep)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to find the absolute path for '%v'!\n",
+		fmt.Fprintf(ErrWriter, "Unable to find the absolute path for '%v'!\n",
 			directoryToSweep)
 		return err
 	}
 
-	fmt.Printf("Sweeping: '%v'\n", absDirectoryToSweep)
+	fmt.Fprintf(OutWriter, "Sweeping: '%v'\n", absDirectoryToSweep)
 	filesToDelete := make([]string, 0)
 	walker := func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "%v\n", err)
+			fmt.Fprintf(ErrWriter, "%v\n", err)
 			return err
 		}
 
 		if info.IsDir() && info.Name() == sdFolderName {
 			sdFolder := path
-			fmt.Printf("Search SD folder '%v'\n", sdFolder)
+			fmt.Fprintf(OutWriter, "Search SD folder '%v'\n", sdFolder)
 			containingFolder := filepath.Dir(sdFolder)
-			fmt.Printf("Containing folder '%v'\n", containingFolder)
+			fmt.Fprintf(OutWriter, "Containing folder '%v'\n", containingFolder)
 
 			sdFiles, err := filepath.Glob(filepath.Join(sdFolder, "*.txt"))
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "%v\n", err)
+				fmt.Fprintf(ErrWriter, "%v\n", err)
 				return err
 			}
 
 			for _, sdFile := range sdFiles {
-				fmt.Printf("SD File '%v'\n", sdFile)
+				fmt.Fprintf(OutWriter, "SD File '%v'\n", sdFile)
 				actionForFile, err := getActionForFile(sdFile, containingFolder)
 				if err != nil {
-					fmt.Fprintf(os.Stderr, "%v\n", err)
+					fmt.Fprintf(ErrWriter, "%v\n", err)
 					return err
 				}
 
 				if actionForFile.action == "delete" {
-					fmt.Printf("Deleting '%v'\n", actionForFile.file)
+					fmt.Fprintf(OutWriter, "Deleting '%v'\n", actionForFile.file)
 					filesToDelete = append(filesToDelete, actionForFile.file)
 				} else if actionForFile.action == "keep" {
-					fmt.Printf("Keeping '%v'\n", actionForFile.file)
+					fmt.Fprintf(OutWriter, "Keeping '%v'\n", actionForFile.file)
 				} else {
-					fmt.Fprintf(os.Stderr, "Unrecognised action '%v' from '%v'!\n",
+					fmt.Fprintf(ErrWriter, "Unrecognised action '%v' from '%v'!\n",
 						actionForFile.action, sdFile)
 				}
 			}
@@ -166,12 +210,12 @@ func sweepDirectory(directoryToSweep string) error {
 
 	err = filepath.Walk(absDirectoryToSweep, walker)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(ErrWriter, "%v\n", err)
 		return err
 	}
 
 	for _, fileToDelete := range filesToDelete {
-		fmt.Printf("Deleting: '%v'\n", fileToDelete)
+		fmt.Fprintf(OutWriter, "Deleting: '%v'\n", fileToDelete)
 		os.RemoveAll(fileToDelete)
 	}
 
@@ -183,7 +227,7 @@ func getActionForFile(sdFileName, containingFolder string) (ActionForFile, error
 	defer sdFile.Close()
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintf(ErrWriter, "%v\n", err)
 		return ActionForFile{"", ""}, err
 	}
 
