7d0a55d5e41bdba5e857ceda8cddc6bb363228d9
Robert Impey <robert-impey@users.noreply.github.com>
2024-01-04 22:57:26 +0000

    Help message for sweepFrom

--- cmd/sweepFrom.go
+++ cmd/sweepFrom.go
@@ -23,13 +23,10 @@ import (
 // sweepFromCmd represents the sweepFrom command
 var sweepFromCmd = &cobra.Command{
 	Use:   "sweepFrom",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sweep from all the directories listed",
+	Long: `The arguments to this command should be text files with
+	one directory per line.
+	Each directory will be swept.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("sweepFrom called")
 	},
